cb73ec271b0430d0f7de138a88d9859616ad7fde
Hiago 7k <hiago7k@gmail.com>
2026-07-06 18:43:57 -0300

    estrutura basica

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 class Main{
     static void main() {
-        System.out.println("hello WOrld");
+        System.out.println("***************************");
+        System.out.println("------- Orange Bank -------");
+        System.out.println("***************************");
+
+
+        System.out.println("-----------------------------");
+        System.out.println("Dados iniciais do cliente: ");
+        System.out.println("-----------------------------");
     }
 }
